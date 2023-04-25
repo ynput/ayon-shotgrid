6a29bd1d9c9778749b49e087cb587e6faf7e0c83
Jakub Trllo <jakub.trllo@gmail.com>
2023-04-25 13:36:03 +0200

    refactored collect entities

--- client/ayon_shotgrid/plugins/publish/collect_shotgrid_entities.py
+++ client/ayon_shotgrid/plugins/publish/collect_shotgrid_entities.py
@@ -70,131 +70,139 @@ class CollectShotgridEntities(pyblish.api.ContextPlugin):
             instance.data["shotgridEntity"] = sg_asset
             instance.data["shotgridTask"] = sg_task
 
-    def _get_sg_tasks_by_id(self, context):
-        """ Get all instances tasks from Shotgrid.
-
-        We loop over all the instances and gather all the tasks so we can
-        perform one Shotgird query and get all Tasks, then build a dict by their
-        IDs.
-
-        Args:
-            context (pyblish.Plugin.context): The current publish context.
-
-        Returns:
-            sg_tasks_by_id (dict): All the found tasks, with their ID as key
-                and the Shotgrid entity dict as value.
-        """
-        sg_session = context.data["shotgridSession"]
-        sg_tasks_ids = []
-        sg_tasks_by_id = {}
-
-        ayon_folder_ids = [
-            instance.data.get("assetEntity").get("_id")
-            for instance in context
-            if instance.data.get("assetEntity").get("_id")
-        ]
-
-        ayon_tasks = list(ayon_api.get_tasks(
-            context.data["projectName"],
-            folder_ids=ayon_folder_ids
-        ))
-
-        for instance in context:
-            ayon_asset = instance.data.get("assetEntity")
-            ayon_task_name = instance.data.get("task")
-            ayon_task = None
-
-            if not ayon_task_name:
-                continue
-
-            for task in ayon_tasks:
-                if (
-                    task['folderId'] == ayon_asset["_id"]
-                    and ayon_task_name == task["name"]
-                ):
-                    ayon_task = task
-
-            if not ayon_task:
-                continue
-
-            sg_task_id = ayon_task.get("attrib", {}).get("shotgridId")
-
-            if not sg_task_id:
-                continue
-
-            sg_tasks_ids.append(int(sg_task_id))
-
-        for sg_task in self._query_sg_by_ids(
-            sg_session,
-            "Task",
-            sg_tasks_ids,
-            query_fields=["content"]
-         ):
-            sg_tasks_by_id[sg_task["id"]] = sg_task
-
-        return sg_tasks_by_id
-
-    def _get_sg_assets_by_id(self, context):
+    def _get_sg_entities_by_id(self, context, sg_session):
         """ Get all instances assets from Shotgrid.
 
         We loop over all the instances and gather all the assets so we can
-        perform one Shotgird query and get all Assets, then build a dict by
+        perform one Shotgrid query and get all Assets, then build a dict by
         their IDs.
 
         Args:
             context (pyblish.Plugin.context): The current publish context.
+            sg_session (shotgun_api3.Shotgun): Connected Shotgrid api session.
 
         Returns:
-            sg_tasks_by_id (dict): All the found Assets, with their ID as key
-                and the Shotgrid entity dict as value.
+            tuple[dict[int, Any], dict[int, Any]]: All the found Assets,
+                with their ID as key and the Shotgrid entity dict as value
+                and found tasks.
         """
-        sg_session = context.data["shotgridSession"]
-        sg_assets_by_type = {}
-        sg_assets_by_id = {}
 
+        folder_ids = set()
+        sg_assets_by_type = collections.defaultdict(set)
+        task_names_by_sg_id = collections.defaultdict(set)
         for instance in context:
-            ayon_asset = instance.data.get("assetEntity")
-            sg_asset_id = ayon_asset.get("data", {}).get("shotgridId")
-            sg_asset_type = ayon_asset.get("data", {}).get("shotgridType")
+            ayon_folder = instance.data.get("assetEntity")
+            if not ayon_folder:
+                continue
 
-            if sg_asset_id and sg_asset_type:
-                sg_assets_by_type.setdefault(sg_asset_type, []) 
-                sg_assets_by_type[sg_asset_type].append(sg_asset_id)
+            task_name = instance.data.get("task")
+            folder_ids.add(ayon_folder["_id"])
+            sg_id = ayon_folder["data"].get("shotgridId")
+            sg_type = ayon_folder["data"].get("shotgridType")
+            if sg_id and sg_type:
+                sg_id = int(sg_id)
+                sg_assets_by_type[sg_type].add(sg_id)
+                task_names_by_sg_id[sg_id].add(task_name)
 
-        for sg_asset_type, sg_asset_ids in sg_assets_by_type.items():
+        sg_assets_by_id = {}
+        for sg_type, sg_ids in sg_assets_by_type.items():
             sg_assets = self._query_sg_by_ids(
                 sg_session,
-                sg_asset_type,
-                sg_asset_ids
+                sg_type,
+                sg_ids,
+                ["type"]
             )
-
             for sg_asset in sg_assets:
                 sg_assets_by_id[sg_asset["id"]] = sg_asset
 
-        return sg_assets_by_id
+        sg_tasks_by_asset_id = self._get_sg_tasks_by_id(
+            sg_session, sg_assets_by_id, task_names_by_sg_id
+        )
+        return sg_assets_by_id, sg_tasks_by_asset_id
 
-    def _query_sg_by_ids(self, sg_session, sg_type, ids_list, query_fields=None):
-        """ Query a list of IDs of a type from Shotgrid.
-        
+    def _get_sg_tasks_by_id(
+        self, sg_session, sg_assets_by_id, task_names_by_sg_id
+    ):
+        """Find tasks in Shotgrid.
+
+        Tasks are found by names and their parent shotgrid ids.
+
         Args:
-            sg_sesion (shotgun_api3.Shotgun): THe Shotgrid session.
-            sg_type (str): The asset type to query.
-            ids_list (list): The list of IDs to query.
+             sg_session (shotgun_api3.Shotgun): THe Shotgrid session.
+             sg_assets_by_id (dict[int, dict[str, Any]]): Queried Shotgrid
+                entities by id.
+            task_names_by_sg_id (dict[int, set[str]]): Task names by Shotgrid
+                entity id.
+
+        Returns:
+            dict[int, dict[str, dict[str, Any]]]: All the found tasks by name
+                and by shotgrid entity id.
         """
+
+        sg_tasks_by_asset_id = {
+            asset_id: {}
+            for asset_id in task_names_by_sg_id
+        }
+
+        filters = []
+        for sg_id, task_names in task_names_by_sg_id.items():
+            sg_entity = sg_assets_by_id.get(sg_id)
+            if not sg_entity:
+                continue
+
+            # Parent id filter need a type in field ('entity.Asset.id')
+            parent_filter_field = "entity.{}.id".format(sg_entity["type"])
+            filters.append({
+                "filter_operator": "all",
+                "filters": [
+                    [parent_filter_field, "is", sg_id],
+                    ["content", "in", list(task_names)]
+                ]
+            })
+
+        if not filters:
+            return sg_tasks_by_asset_id
+
+        sg_tasks = sg_session.find(
+            "Task",
+            filters=filters,
+            fields=["content", "entity"],
+            filter_operator="any"
+        )
+        for sg_task in sg_tasks:
+            parent_id = sg_task["entity"]["id"]
+            task_name = sg_task["content"]
+            sg_tasks_by_asset_id[parent_id][task_name] = sg_task
+
+        return sg_tasks_by_asset_id
+
+    def _query_sg_by_ids(
+        self, sg_session, sg_type, sg_asset_ids, query_fields=None
+    ):
+        """ Query a list of IDs of a type from Shotgrid.
+
+        Args:
+            sg_session (shotgun_api3.Shotgun): THe Shotgrid session.
+            sg_type (str): The asset type to query.
+            sg_asset_ids (set[int]): The list of IDs to query.
+            query_fields (Optional[list[str]]): Fields to get.
+        """
+
+        if not sg_asset_ids:
+            return []
+
         if not query_fields:
             query_fields = []
 
         query_dict = {
-                "filter_operator": "any",
-                "filters": []
-            }
-
-        for sg_id in ids_list:
-            query_dict["filters"].append(["id", "is", int(sg_id)])
+            "filter_operator": "any",
+            "filters": [
+                ["id", "in", list(sg_asset_ids)]
+            ]
+        }
 
         return sg_session.find(
             sg_type,
             filters=[query_dict],
             fields=query_fields,
         )
-
