555387bb3f0f1a6e974c9e94b5196e2c6dd5ac1f
Oscar Domingo <oscardomingo@gmail.com>
2023-06-21 15:50:09 +0100

    Convert into a Rez package
    This commit add the `package.py` and `rezbuild.py` files necessary to
    allow rez to build and release it.

---
+++ rezbuild.py
@@ -0,0 +1,214 @@
+"""Prepares server package from addon repo to upload to server.
+
+Requires Python 3.9. (Or at least 3.8+).
+
+This script should be called from cloned addon repo.
+
+It will produce 'package' subdirectory which could be pasted into server
+addon directory directly (eg. into `ayon-backend/addons`).
+
+Format of package folder:
+ADDON_REPO/package/{addon name}/{addon version}
+
+You can specify `--output_dir` in arguments to change output directory where
+package will be created. Existing package directory will always be purged if
+already present! This could be used to create package directly in server folder
+if available.
+
+Package contains server side files directly,
+client side code zipped in `private` subfolder.
+"""
+
+import os
+import sys
+import re
+import shutil
+import argparse
+import logging
+import collections
+import zipfile
+
+# Name of addon
+#   - e.g. 'maya'
+ADDON_NAME = "shotgrid"
+# Name of folder where client code is located to copy 'version.py'
+#   - e.g. 'ayon_maya'
+ADDON_CLIENT_DIR = "ayon_shotgrid"
+
+# Patterns of directories to be skipped for server part of addon
+IGNORE_DIR_PATTERNS = [
+    re.compile(pattern)
+    for pattern in {
+        # Skip directories starting with '.'
+        r"^\.",
+        # Skip any pycache folders
+        "^__pycache__$"
+    }
+]
+
+# Patterns of files to be skipped for server part of addon
+IGNORE_FILE_PATTERNS = [
+    re.compile(pattern)
+    for pattern in {
+        # Skip files starting with '.'
+        # NOTE this could be an issue in some cases
+        r"^\.",
+        # Skip '.pyc' files
+        r"\.pyc$"
+    }
+]
+
+
+def safe_copy_file(src_path, dst_path):
+    """Copy file and make sure destination directory exists.
+
+    Ignore if destination already contains directories from source.
+
+    Args:
+        src_path (str): File path that will be copied.
+        dst_path (str): Path to destination file.
+    """
+
+    if src_path == dst_path:
+        return
+
+    dst_dir = os.path.dirname(dst_path)
+    try:
+        os.makedirs(dst_dir)
+    except Exception:
+        pass
+
+    shutil.copy2(src_path, dst_path)
+
+
+def _value_match_regexes(value, regexes):
+    for regex in regexes:
+        if regex.search(value):
+            return True
+    return False
+
+
+def find_files_in_subdir(
+    src_path,
+    ignore_file_patterns=None,
+    ignore_dir_patterns=None
+):
+    if ignore_file_patterns is None:
+        ignore_file_patterns = IGNORE_FILE_PATTERNS
+
+    if ignore_dir_patterns is None:
+        ignore_dir_patterns = IGNORE_DIR_PATTERNS
+    output = []
+
+    hierarchy_queue = collections.deque()
+    hierarchy_queue.append((src_path, []))
+    while hierarchy_queue:
+        item = hierarchy_queue.popleft()
+        dirpath, parents = item
+        for name in os.listdir(dirpath):
+            path = os.path.join(dirpath, name)
+            if os.path.isfile(path):
+                if not _value_match_regexes(name, ignore_file_patterns):
+                    items = list(parents)
+                    items.append(name)
+                    output.append((path, os.path.sep.join(items)))
+                continue
+
+            if not _value_match_regexes(name, ignore_dir_patterns):
+                items = list(parents)
+                items.append(name)
+                hierarchy_queue.append((path, items))
+
+    return output
+
+
+def copy_server_content(addon_output_dir, current_dir, log):
+    """Copies server side folders to 'addon_package_dir'
+
+    Args:
+        addon_output_dir (str): package dir in addon repo dir
+        current_dir (str): addon repo dir
+        log (logging.Logger)
+    """
+
+    log.info(f"Copying server content:\n{current_dir} -> {addon_output_dir}")
+
+    filepaths_to_copy = []
+    server_dirpath = os.path.join(current_dir, "server")
+
+    # Version
+    src_version_path = os.path.join(current_dir, "version.py")
+    dst_version_path = os.path.join(addon_output_dir, "version.py")
+    filepaths_to_copy.append((src_version_path, dst_version_path))
+
+
+    for item in find_files_in_subdir(server_dirpath):
+        src_path, dst_subpath = item
+        dst_path = os.path.join(addon_output_dir, dst_subpath)
+        filepaths_to_copy.append((src_path, dst_path))
+
+    # Copy files
+    for src_path, dst_path in filepaths_to_copy:
+        safe_copy_file(src_path, dst_path)
+
+
+def zip_client_side(addon_package_dir, current_dir, log):
+    """Copy and zip `client` content into 'addon_package_dir'.
+
+    Args:
+        addon_package_dir (str): Output package directory path.
+        current_dir (str): Directory path of addon source.
+        log (logging.Logger): Logger object.
+    """
+
+    client_dir = os.path.join(current_dir, "client")
+    if not os.path.isdir(client_dir):
+        log.info("Client directory was not found. Skipping")
+        return
+
+    log.info("Preparing client code zip")
+    private_dir = os.path.join(addon_package_dir, "private")
+
+    if not os.path.exists(private_dir):
+        os.makedirs(private_dir)
+
+    src_version_path = os.path.join(current_dir, "version.py")
+    dst_version_path = os.path.join(ADDON_CLIENT_DIR, "version.py")
+
+    zip_filepath = os.path.join(os.path.join(private_dir, "client.zip"))
+    with zipfile.ZipFile(zip_filepath, "w", zipfile.ZIP_DEFLATED) as zipf:
+        # Add client code content to zip
+        for path, sub_path in find_files_in_subdir(client_dir):
+            zipf.write(path, sub_path)
+
+        # Add 'version.py' to client code
+        zipf.write(src_version_path, dst_version_path)
+
+
+def main():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    log = logging.getLogger("create_package")
+
+    release = int(os.environ['REZ_BUILD_INSTALL'])
+
+    output_dir = os.path.join(os.environ["REZ_BUILD_SOURCE_PATH"], "build")
+
+    if release:
+        output_dir = os.environ["REZ_BUILD_INSTALL_PATH"]
+
+    log.info(f"Building into: {output_dir}")
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    else:
+        log.warning(f"Wiping contents of {output_dir}")
+        shutil.rmtree(output_dir)
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    copy_server_content(output_dir, current_dir, log)
+    zip_client_side(output_dir, current_dir, log)
+
+
+if __name__ == "__main__":
+    main()
+
