68cd7143dd8295c4e1488133898abbabfd643f13
Oscar Domingo <oscardomingo@gmail.com>
2023-10-10 12:36:12 +0100

    `client` Rework Shotgrid login Since we now use the secrets in the server side, we can no longer get the api key to login, so we need to ask que user for their user and password; this commit makes the changes so we store the login as a tuple of (`username`, `password`) in the `OpenPypeSettingsRegistry`, and use that entry to login with Shotgrid.

--- client/ayon_shotgrid/tray/sg_login_dialog.py
+++ client/ayon_shotgrid/tray/sg_login_dialog.py
@@ -7,7 +7,7 @@ from openpype import style
 from openpype import resources
 from qtpy import QtCore, QtWidgets, QtGui
 
-class ChangeSgUsername(QtWidgets.QDialog):
+class SgLoginDialog(QtWidgets.QDialog):
     """A QDialog that allows the person to set a Shotgrid Username.
 
     It also allows them to test the username agains the API.
@@ -16,10 +16,10 @@ class ChangeSgUsername(QtWidgets.QDialog):
     dialog_closed = QtCore.Signal()
 
     def __init__(self, module, parent=None):
-        super(ChangeSgUsername, self).__init__(parent)
+        super(SgLoginDialog, self).__init__(parent)
         self.module = module
 
-        self.setWindowTitle("Ayon - Shotgrid Username")
+        self.setWindowTitle("Ayon - Shotgrid Login")
         icon = QtGui.QIcon(resources.get_openpype_icon_filepath())
         self.setWindowIcon(icon)
 
@@ -29,7 +29,7 @@ class ChangeSgUsername(QtWidgets.QDialog):
         )
 
         self.setStyleSheet(style.load_stylesheet())
-        self.setContentsMargins(20, 10, 20, 10)
+        self.setContentsMargins(2, 2, 2, 2)
 
         self.setup_ui()
 
@@ -37,62 +37,60 @@ class ChangeSgUsername(QtWidgets.QDialog):
         """Clear any message when closing the dialog."""
         self.sg_connection_message.setText("")
         self.dialog_closed.emit()
-        super(ChangeSgUsername, self).closeEvent(event)
+        super(SgLoginDialog, self).closeEvent(event)
 
     def setup_ui(self):
         server_url = self.module.get_sg_url()
 
         if not server_url:
             server_url = "No Shotgrid Server set in Ayon Settings."
-
+        
         sg_server_url_label = QtWidgets.QLabel(
-            "Server:\n{}".format(server_url)
+            "Please provide the credentials to log in into the Shotgrid Server:\n{}".format(server_url)
         )
-        self.sg_username_label = QtWidgets.QLabel("Username:")
 
         dialog_layout = QtWidgets.QVBoxLayout()
         dialog_layout.addWidget(sg_server_url_label)
-        dialog_layout.addWidget(self.sg_username_label)
+
+        sg_username, sg_password = credentials.get_local_login()
 
         self.sg_username_input = QtWidgets.QLineEdit()
-        self.sg_username_set_button = QtWidgets.QPushButton("Set")
-        self.sg_username_set_button.clicked.connect(self.set_local_login)
-
-        sg_username = credentials.get_local_login()
 
         if sg_username:
             self.sg_username_input.setText(sg_username)
         else:
             self.sg_username_input.setPlaceholderText("jane.doe@mycompany.com")
 
-        username_layout = QtWidgets.QHBoxLayout()
-        username_layout.setContentsMargins(0, 0, 0, 20)
-        username_layout.addWidget(self.sg_username_input)
-        username_layout.addWidget(self.sg_username_set_button)
+        self.sg_password_input = QtWidgets.QLineEdit()
+        self.sg_password_input.setEchoMode(QtWidgets.QLineEdit.Password)
 
-        dialog_layout.addLayout(username_layout)
+        if sg_password:
+            self.sg_password_input.setText(sg_password)
+        else:
+            self.sg_password_input.setPlaceholderText("c0mPre$Hi0n")
 
-        self.sg_check_login_label= QtWidgets.QLabel((
-            "Use the button below to check if the provided\n"
-            "user has access to the Shotgird API.")
-        )
-        self.sg_check_login_button = QtWidgets.QPushButton("Check Shotgrid Login")
+        dialog_layout.addWidget(QtWidgets.QLabel("Shotgrid Username:"))
+        dialog_layout.addWidget(self.sg_username_input)
+
+        dialog_layout.addWidget(QtWidgets.QLabel("Shotgrid Password:"))
+        dialog_layout.addWidget(self.sg_password_input)
+
+        self.sg_check_login_button = QtWidgets.QPushButton("Login into Shotgrid...")
         self.sg_check_login_button.clicked.connect(self.check_sg_credentials)
         self.sg_connection_message = QtWidgets.QLabel("")
 
-        dialog_layout.addWidget(self.sg_check_login_label)
         dialog_layout.addWidget(self.sg_check_login_button)
         dialog_layout.addWidget(self.sg_connection_message)
-
 
         self.setLayout(dialog_layout)
 
     def set_local_login(self):
         """Change Username label, save in local registry and set env var."""
         sg_username = self.sg_username_input.text()
-        
-        if sg_username:
-            credentials.save_local_login(sg_username)
+        sg_password = self.sg_password_input.text()
+
+        if sg_username and sg_password:
+            credentials.save_local_login(sg_username, sg_password)
             os.environ["AYON_SG_USERNAME"] = sg_username
         else:
             credentials.clear_local_login()
@@ -101,12 +99,17 @@ class ChangeSgUsername(QtWidgets.QDialog):
     def check_sg_credentials(self):
         """Check if the provided username can login via the API."""
         sg_username = self.sg_username_input.text()
+        sg_password = self.sg_password_input.text()
+
         login_result, login_message = credentials.check_user_permissions(
             self.module.get_sg_url(),
-            self.module._shotgrid_script_name,
-            self.module._shotgrid_api_key,
             sg_username,
+            sg_password,
         )
 
         self.set_local_login()
-        self.sg_connection_message.setText(login_message)
+
+        if login_result:
+            self.close()
+        else:
+            self.sg_connection_message.setText(login_message)
