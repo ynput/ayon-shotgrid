682b879c126543b39d1d44020f3a470a40d33c9c
Oscar Domingo <oscardomingo@gmail.com>
2023-10-25 12:47:48 +0100

    `services` Fix variable typo in `__main__` I mistyped "shotgird" in the initial implementation, this commit fixes the three instances of the typo.

--- services/leecher/leecher/__main__.py
+++ services/leecher/leecher/__main__.py
@@ -4,6 +4,6 @@ from .listener import ShotgridListener
 
 
 if __name__ == "__main__":
-    shotgird_listener = ShotgridListener()
-    sys.exit(shotgird_listener.start_listening())
+    shotgrid_listener = ShotgridListener()
+    sys.exit(shotgrid_listener.start_listening())
 
