4f19aa9191267fb193c63e724b1d4cec2663511b
Fabia Serra Arrizabalaga <farrizabalaga@alkemy-x.com>
2023-12-01 11:37:36 -0500

    `client` Strip version number from PublishedFiles so they are grouped together and set basic published file type functionality

--- client/ayon_shotgrid/plugins/publish/integrate_shotgrid_publish.py
+++ client/ayon_shotgrid/plugins/publish/integrate_shotgrid_publish.py
@@ -1,4 +1,5 @@
 import os
+import re
 import platform
 
 import pyblish.api
@@ -17,6 +18,12 @@ class IntegrateShotgridPublish(pyblish.api.InstancePlugin):
     label = "Shotgrid Published Files"
 
     def process(self, instance):
+        # Skip execution if instance is marked to be processed in the farm
+        if instance.data.get("farm"):
+            self.log.info(
+                "Instance is marked to be processed on farm. Skipping")
+            return
+        
         sg_session = instance.context.data.get("shotgridSession")
         sg_version = instance.data.get("shotgridVersion")
 
@@ -36,14 +43,22 @@ class IntegrateShotgridPublish(pyblish.api.InstancePlugin):
                 instance, representation, False
             )
 
-            if representation.get("tags", []):
-                continue
-
             sg_project = instance.data.get("shotgridProject")
             sg_entity = instance.data.get("shotgridEntity")
             sg_task = instance.data.get("shotgridTask")
 
             code = os.path.basename(local_path)
+            # Extract and remove version number from code so Publishedfile versions are
+            # grouped together. More info about this on:
+            # https://developer.shotgridsoftware.com/tk-core/_modules/tank/util/shotgun/publish_creation.html
+            version_number = 0
+            match = re.search("_v(\d+)", code)
+            if match:
+                version_number = int(match.group(1))
+                # Remove version from name
+                code = re.sub("_v\d+", "", code)
+                # Remove frames from name (i.e., filename.1001.exr -> filename.exr)
+                code = re.sub("\.\d+", "", code)
 
             query_filters = [
                 ["project", "is", sg_project],
@@ -60,41 +75,41 @@ class IntegrateShotgridPublish(pyblish.api.InstancePlugin):
                 query_filters
             )
 
-            sg_local_store = sg_session.find_one(
+            sg_local_storage = sg_session.find_one(
                 "LocalStorage",
                 filters=[],
-                fields=["mac_path","windows_path", "linux_path"]
+                fields=["mac_path", "windows_path", "linux_path"]
             )
 
-            if not sg_local_store:
+            if not sg_local_storage:
                 KnownPublishError(
-                    "Unable to find a Local Store in Shotgrid."
+                    "Unable to find a Local Storage in Shotgrid."
                     "Enable them in Site Preferences > Local Management:"
                     "https://help.autodesk.com/view/SGSUB/ENU/?guid=SG_Administrator_ar_data_management_ar_linking_local_files_html"
                 )
 
-            self.log.debug("Using the Local Store: {sg_local_store}")
+            self.log.debug("Using the Local Storage: {sg_local_storage}")
 
             try:
                 if platform.system() == "Windows":
                     _, file_partial_path = local_path.split(
-                        sg_local_store["windows_path"]
+                        sg_local_storage["windows_path"]
                     )
                     file_partial_path = file_partial_path.replace("\\", "/")
                 elif platform.system() == "Linux":
                     _, file_partial_path = local_path.split(
-                        sg_local_store["linux_path"]
+                        sg_local_storage["linux_path"]
                     )
                 elif platform.system() == "Darwin":
                     _, file_partial_path = local_path.split(
-                        sg_local_store["mac_path"]
+                        sg_local_storage["mac_path"]
                     )
 
                 file_partial_path = file_partial_path.lstrip("/")
             except ValueError:
                 raise KnownPublishError(
                     f"Filepath {local_path} doesn't match the "
-                    f"Shotgrid Local Store {sg_local_store}"
+                    f"Shotgrid Local Storage {sg_local_storage}"
                     "Enable them in Site Preferences > Local Management:"
                     "https://help.autodesk.com/view/SGSUB/ENU/?guid=SG_Administrator_ar_data_management_ar_linking_local_files_html"
                 )
@@ -105,9 +120,14 @@ class IntegrateShotgridPublish(pyblish.api.InstancePlugin):
                 "entity": sg_entity,
                 "version": sg_version,
                 "path": {
-                    "local_storage": sg_local_store,
+                    "local_storage": sg_local_storage,
                     "relative_path": file_partial_path
                 },
+                # Add file type and version number fields
+                "published_file_type": self._find_published_file_type(
+                    instance, local_path, representation
+                ),
+                "version_number": version_number,
             }
 
             if sg_task:
@@ -148,3 +168,43 @@ class IntegrateShotgridPublish(pyblish.api.InstancePlugin):
                 )
             instance.data["shotgridPublishedFile"] = sg_published_file
 
+    def _find_published_file_type(self, instance, filepath, representation):
+        """Given a filepath infer what type of published file type it is."""
+
+        _, ext = os.path.splitext(filepath)
+        published_file_type = "Unknown"
+
+        if ext in [".exr", ".jpg", ".jpeg", ".png", ".dpx", ".tif", ".tiff"]:
+            is_sequence = len(representation["files"]) > 1
+            if is_sequence:
+                published_file_type = "Rendered Image"
+            else:
+                published_file_type = "Image"
+        elif ext in [".mov", ".mp4"]:
+            published_file_type = "Movie"
+        elif ext == ".abc":
+            published_file_type = "Alembic Cache"
+        elif ext in [".bgeo", ".sc", ".gz"]:
+            published_file_type = "Bgeo Geo"
+        elif ext in [".ma", ".mb"]:
+            published_file_type = "Maya Scene"
+        elif ext == ".nk":
+            published_file_type = "Nuke Script"
+        elif ext == ".hip":
+            published_file_type = "Houdini Scene"
+        elif ext in [".hda"]:
+            published_file_type = "HDA"
+        elif ext in [".fbx"]:
+            published_file_type = "FBX Geo"
+
+        filters = [["code", "is", published_file_type]]
+        sg_session = instance.context.data.get("shotgridSession")
+        sg_published_file_type = sg_session.find_one(
+            "PublishedFileType", filters=filters
+        )
+        if not sg_published_file_type:
+            # Create a published file type on the fly
+            sg_published_file_type = sg_session.create(
+                "PublishedFileType", {"code": published_file_type}
+            )
+        return sg_published_file_type
