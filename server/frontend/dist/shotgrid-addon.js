df147f734b40e81d1b644ef9e3af91c772f81fb5
Fabià Serra Arrizabalaga <fabia.serra92@gmail.com>
2024-03-29 16:59:30 +0100

    Remove projects filtering from PR

--- server/frontend/dist/shotgrid-addon.js
+++ server/frontend/dist/shotgrid-addon.js
@@ -296,19 +296,12 @@ const getShotgridProjects = async () => {
 
   if (sgProjects) {
     sgProjects.forEach((project) => {
-      /* Only add projects that don't contain whitespaces in the name
-      and have a code name as those are the requirements to sync to Ayon. */
-      if (
-        !project.attributes.name.includes(" ") &&
-        project.attributes[`${addonSettings.shotgrid_project_code_field}`]
-      ) {
-        sgProjectsConformed.push({
-          "name": project.attributes.name,
-          "code": project.attributes[`${addonSettings.shotgrid_project_code_field}`],
-          "shotgridId": project.id,
-          "ayonId": project.attributes.sg_ayon_id,
-        })
-      }
+      sgProjectsConformed.push({
+        "name": project.attributes.name,
+        "code": project.attributes[`${addonSettings.shotgrid_project_code_field}`],
+        "shotgridId": project.id,
+        "ayonId": project.attributes.sg_ayon_id,
+      })
     });
   }
   return sgProjectsConformed;
