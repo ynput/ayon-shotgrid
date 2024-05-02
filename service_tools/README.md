3fab85d9cb88b0dcdfb5bee93d54d54c0f4451d3
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-02 14:31:04 +0200

    implemented simple service tools to run them out of docker

---
+++ service_tools/README.md
@@ -0,0 +1,25 @@
+## Service tools
+Helper tools to develop shotgrid services or parts of the services.
+
+### How to run
+At this moment there is available PowerShell script and Makefile. All commands expect that there is created virtual environment in `./venv`. These scripts depend on the existence of a `./.env`, use `example_env` as template. The contents of the file should be:
+```
+AYON_SERVER_URL={AYON server url}
+AYON_API_KEY={AYON server api key (ideally service user)}
+```
+
+### Commands
+- `install` - install requirements needed for running processed (requires Git)
+- `leecher` - start leecher
+- `processor` - start processor
+- `transmitter` - start transmitter
+- `services` - start all services
+
+### Leecher 
+Shotgrid leecher postpone shotgrid events into AYON event database. Is separated from processor to be able restart or have different shotgrid processors for different purposes loading events from single place. Using AYON server as middle-ware helps to know which event was already processed or is processing. In theory one event should not be processed multiple times. 
+
+### Processor
+Processor of shotgrid events. Is not loading events from shotgrid but from AYON database. Can get only one shotgrid event at once and if there is other running processor processing events under same identifier it won't continue to process next events until that is finished. That is due to race condition issues that may happen. Processor requires to have running **leecher**.
+
+### Transmitter
+Transmit AYON events to shotgrid.
---
+++ service_tools/manage.ps1
@@ -0,0 +1,109 @@
+# Receive first positional argument
+$FunctionName=$ARGS[0]
+$arguments=@()
+if ($ARGS.Length -gt 1) {
+    $arguments = $ARGS[1..($ARGS.Length - 1)]
+}
+
+$script_dir_rel = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$script_dir = (Get-Item $script_dir_rel).FullName
+
+$ADDON_VERSION = Invoke-Expression -Command "python -c ""import os;import sys;content={};f=open(r'$($script_dir)/../package.py');exec(f.read(),content);f.close();print(content['version'])"""
+
+function Default-Func {
+  Write-Host ""
+  Write-Host "*************************"
+  Write-Host "AYON shotgrid services tool"
+  Write-Host "   Run shotgrid services"
+  Write-Host "*************************"
+  Write-Host ""
+  Write-Host "Run service processes from terminal. It is recommended to use docker images for production."
+  Write-Host ""
+  Write-Host "Usage: start [target]"
+  Write-Host ""
+  Write-Host "Optional arguments for service targets:"
+  Write-Host "--variant [variant] (Define settings variant. default: 'production')"
+  Write-Host ""
+  Write-Host "Runtime targets:"
+  Write-Host "  create-env    Install requirements to currently actie python (recommended to create venv)"
+  Write-Host "  leecher       Start leecher of shotgrid events"
+  Write-Host "  processor     Main processing logic"
+  Write-Host "  transmitter   Transmit AYON events to shotgrid"
+  Write-Host "  services      Start both leecher and processor (experimental)"
+  Write-Host ""
+}
+
+function Install-Requirements {
+  # TODO Install/verify venv is created
+  & python -m pip install -r "$($script_dir)\requirements.txt"
+}
+
+function Start-Leecher {
+  & python "$($script_dir)\main.py" --service leecher @arguments
+}
+
+function Start-Processor {
+  & python "$($script_dir)\main.py" --service processor @arguments
+}
+
+function Start-Transmitter {
+  & python "$($script_dir)\main.py" --service transmitter @arguments
+}
+
+function Start-All {
+  & python "$($script_dir)\main.py" --service all @arguments
+}
+
+function Load-Env {
+  $env_path = "$($script_dir)/.env"
+  if (Test-Path $env_path) {
+    Get-Content $env_path | foreach {
+      $name, $value = $_.split("=")
+      if (-not([string]::IsNullOrWhiteSpace($name) -or $name.Contains("#"))) {
+        Set-Content env:\$name $value
+      }
+    }
+  }
+}
+
+function Activate-Venv {
+  # Make sure venv is created
+  $venv_path = "$($script_dir)\.venv"
+  if (-not(Test-Path $venv_path)) {
+    & python -m venv $venv_path
+  }
+  & "$($venv_path)\Scripts\activate.ps1"
+}
+
+function main {
+  if ($null -eq $FunctionName) {
+    Default-Func
+    return
+  }
+  $FunctionName = $FunctionName.ToLower() -replace "\W"
+  $env:AYON_ADDON_NAME = "shotgrid"
+  $env:AYON_ADDON_VERSION = $ADDON_VERSION
+  Load-Env
+  Activate-Venv
+
+  try {
+    if ($FunctionName -eq "createenv") {
+      Install-Requirements
+    } elseif ($FunctionName -eq "leecher") {
+      Start-Leecher
+    } elseif ($FunctionName -eq "processor") {
+      Start-Processor
+    } elseif ($FunctionName -eq "transmitter") {
+      Start-Transmitter
+    } elseif ($FunctionName -eq "services") {
+      Start-All
+    } else {
+      Write-Host "Unknown function ""$FunctionName"""
+      Default-Func
+    }
+  } finally {
+    & deactivate
+  }
+}
+
+main
