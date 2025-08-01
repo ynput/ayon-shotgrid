08f28275e68abb3e5a2b8ca3fcfb4d2d0950318b
Petr Kalis <petr.kalis@gmail.com>
2025-08-01 16:46:45 +0200

    Added collector to mark representation

---
+++ client/ayon_shotgrid/plugins/publish/extract_movie_path_representation.py
@@ -0,0 +1,54 @@
+import pyblish.api
+
+from ayon_core.lib import filter_profiles
+from ayon_shotgrid import MoviePathTrait
+
+
+class ExtractMoviePath(pyblish.api.InstancePlugin):
+    """Looks for representation to be marked for source of sg_movie_path"""
+    order = pyblish.api.ExtractorOrder + 0.45
+    label = "Extract trait for representation for sg_movie_path"
+    targets = ["local"]
+    settings_category = "shotgrid"
+
+    profiles = []
+
+    def process(self, instance):
+        profile = self._get_representation_profile(instance)
+        if not profile:
+            self.log.debug(
+                (
+                    "Skipped instance. None of profiles matched in presets."
+                )
+            )
+            return
+
+        traits = {}
+        for representation in instance.data.get("representations", []):
+            if representation["name"] in profile["repre_names"]:
+                traits[representation["name"]] = MoviePathTrait()
+
+        if traits:
+            instance.data["traits"] = traits
+
+    def _get_representation_profile(self, instance):
+        host_name = instance.context.data["hostName"]
+        product_type = instance.data["productType"]
+        task_name = None
+        task_type = None
+        task_entity = instance.data.get("taskEntity")
+        if task_entity:
+            task_type = task_entity["taskType"]
+            task_name = task_entity["name"]
+
+        profile = filter_profiles(
+            self.profiles,
+            {
+                "hosts": host_name,
+                "product_types": product_type,
+                "task_names": task_name,
+                "task_types": task_type,
+            },
+            logger=self.log,
+        )
+        return profile
